ad31489f6f63c476fec7b7387a00bd71ccd92876
ryankawahara <64173139+ryankawahara@users.noreply.github.com>
2023-04-20 00:50:27 -0400

    Update youtube_test.py

--- youtube_test.py
+++ youtube_test.py
@@ -1,5 +1,5 @@
 # importing the module
-from pytube import YouTube
+from pytube import YouTube, exceptions
 import os
 from moviepy.video.io.VideoFileClip import VideoFileClip
 import time
@@ -63,4 +63,61 @@ def separate_audio(video_path, audio_path):
         return True
     return False
 
-download_video("n2Y3GoN2PGw")
+
+def get_ids(source):
+    with open(source, 'r') as file:
+        lines = file.readlines()
+    list_items = [line.strip() for line in lines]
+    return list_items
+
+def check_ids(ids):
+    unavail_ids = []
+    total = len(ids)
+    for index, id in enumerate(ids):
+        link = f"https://www.youtube.com/watch?v={id}"
+
+        try:
+            # object creation using YouTube
+            # which was imported in the beginning
+            yt = YouTube(link)
+        except:
+            print("Connection Error")  # to handle exception
+        print(f"{id}    {index}/{total}")
+
+        success = False
+        fail_count = 0
+        while success == False and fail_count < 8:
+            try:
+                mp4files = yt.streams.filter(file_extension='mp4', resolution='360p')
+                success = True
+            except KeyError:
+                print("retrying")
+                success = False
+                fail_count += 1
+            except exceptions.VideoUnavailable:
+                print(f"{id} is unavailable")
+                unavail_ids.append(id)
+                unavail = True
+                break
+
+    revised_ids = [x for x in ids if x not in unavail_ids]
+    with open('avail.txt', 'w') as f:
+        f.write('\n'.join(revised_ids))
+        # f.write(str(revised_ids))
+
+    with open('unavail.txt', 'w') as f:
+        f.write('\n'.join(unavail_ids))
+
+        # f.write(str(unavail_ids))
+
+    return unavail_ids
+
+
+
+        # filters out all the files with "mp4" extension and 360p resolution
+
+
+# download_video("n2Y3GoN2PGw")
+ids = get_ids("ids.txt")
+unavail = check_ids(ids)
+print(unavail)
